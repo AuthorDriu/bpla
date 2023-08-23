5098a0c5847bf43b1c2f9eb2795411534d2eda15
Andrew <authordriu@yandex.ru>
2023-08-24 01:35:43 +0300

    First version of BPLA API. I don't know if it works

---
+++ bpla_server.py
@@ -0,0 +1,136 @@
+from flask import Flask, redirect, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+import links
+
+
+app = Flask(__name__)
+
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+db.init_app(app=app)
+
+
+# MODELS ###############################################################
+
+class Users(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    middle_name = db.Column(db.String(50))
+    education_place = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"<User id:{self.id}>"
+    
+class Results(db.Model):
+    __tablename__ = "itog"
+
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(50), nullable=False, unique=True)
+    score = db.Column(db.Integer, nullable=True)
+    time_spent = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return f"<Result id:{self.id}>"
+
+
+# API #################################################################
+
+@app.route("/create_user", methods=["POST",])
+def create_user():
+    try:
+        first_name = request.form["first-name"]
+        last_name = request.form["last-name"]
+        try:
+            middle_name = request.form["middle-name"]
+        except KeyError:
+            middle_name = None
+        phone_number = request.form["phone-number"]
+        education_place = request.form["education"]
+
+        print("Регистрация: {} {}{} {} {}".format(last_name, first_name, " " + middle_name if middle_name is not None else "", education_place, phone_number))
+
+        new_user = Users()
+        try:
+            db.session.add(new_user)
+            new_user.first_name = first_name
+            new_user.last_name = last_name
+            new_user.middle_name = middle_name
+            new_user.education_place = education_place
+            new_user.phone_number = phone_number
+            db.session.flush()
+        except Exception:
+            return redirect(links.REGISTRATION_FAILURE_URL)
+        db.session.commit()
+
+        return redirect(links.REGISTRATION_SUCCESS_URL)
+    except Exception:
+        return redirect(links.REGISTRATION_FAILURE_URL)
+
+
+@app.route("/set_result", methods=["POST"])
+def set_result():
+    phone_number = request.form["phone-number"]
+    score = request.form["score"]
+    time_spent = request.form["time-spent"]
+
+    user = db.session.query(Users).filter_by(phone_number=phone_number).one()
+    if user is None: return jsonify({
+        "success": False
+    })
+
+    result = Results()
+    try:
+        db.session.add(result)
+        result.phone_number = phone_number
+        result.score = score
+        result.time_spent = time_spent
+        db.session.flush()
+    except Exception:
+        return jsonify({
+            "success": False
+        })
+    db.session.commit()
+    return jsonify({
+        "success": True
+    })
+
+
+# CHECK #################################################################
+
+@app.route("/is_a_user", methods=["GET",])
+def is_a_user():
+    phone_number = request.args["phone-number"]
+    results = db.session.query(Users).filter_by(phone_number=phone_number).one()
+    if results is None:
+        return jsonify({
+            "is-a-user": False
+        })
+    else:
+        return jsonify({
+            "is-a-user": True
+        })
+
+@app.route("/has_passed", methods=["GET",])
+def is_user_passed():
+    phone_number = request.args["phone-number"]
+    results = db.session.query(Results).filter_by(phone_number=phone_number).one()
+    if results is None:
+        return jsonify({
+            "has-passed": False
+        })
+    else:
+        return jsonify({
+            "has-passed": True
+        })
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run("0.0.0.0", 5000, debug=True)
---
+++ links.py
@@ -0,0 +1,2 @@
+REGISTRATION_SUCCESS_URL = "http://xn--92-9kc0b3d.xn--p1ai/%d1%83%d1%81%d0%bf%d0%b5%d1%85/"
+REGISTRATION_FAILURE_URL = "http://xn--92-9kc0b3d.xn--p1ai/%d0%bf%d1%80%d0%be%d0%b2%d0%b0%d0%bb/"
